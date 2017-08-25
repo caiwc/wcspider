8c5947577daf9569d4c0aac64b9f013771445804
蔡伟城 <caiweicheng@caiweichengdeMacBook-Air.local>
2017-08-25 14:43:33 +0800

    增加异步功能

--- crawl.py
+++ crawl.py
@@ -1,30 +1,46 @@
-import requests
+import asyncio
+import aiohttp
 
 
-class BaseSpider(object):
-    def __init__(self, url, method):
-        self.url = url
-        self.method = method.upper()
+class Spider(object):
+    def __init__(self, num=3, url_list=None):
+        self.queue = asyncio.Queue()
+        self.result = []
+        self.sema = asyncio.Semaphore(num)
+        self.url_list = url_list
 
-    def __call__(self, *args, **kwargs):
-        text = self.Crawl()
-        self.Analyze(text)
-        self.Save()
+    async def fetch_async(self, method):
+        while True:
+            url = await self.queue.get()
+            with (await self.sema):
+                async with aiohttp.request(method=method, url='http://httpbin.org/get?a={}'.format(url)) as r:
+                    print("get {}...".format(url))
+                    data = await r.json()
 
-    def Crawl(self):
-        print("crawling--"+self.url)
-        res = requests.request(url=self.url, method=self.method)
-        return res.text
+            self.queue.task_done()
+            await self.analysis(data)
 
-    def Analyze(self,text):
-        print("do analysis")
-        pass
+    async def analysis(self, data):
+        res = {
+            "arg":data["args"]["a"],
+            "origin":data["origin"],
+            "url":data["url"]
+        }
+        self.result.append(res)
 
-    def Save(self):
-        print("do save")
-        pass
+    async def start(self, item):
+        fetch = asyncio.ensure_future(self.fetch_async('GET'))
+        await self.queue.put(item)
+        await self.queue.join()
+        fetch.cancel()
 
+    def __run__(self):
+        loop = asyncio.get_event_loop()
+        f = asyncio.wait([self.start(num) for num in self.url_list])
+        loop.run_until_complete(f)
+        loop.close()
+        print(self.result)
 
 if __name__ == '__main__':
-    b =BaseSpider('https://www.baidu.com/','get')
-    b.__call__()
+    s = Spider(url_list=[1, 5, 6, 73, 4])
+    s.__run__()
